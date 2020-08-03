391b994184a82364989aa90cbb73ac0619a531dd
Abzed <ibraabzed@gmail.com>
2020-08-03 18:17:08 +0300

    remove plain crisp and extra large pizza, link pizza size to js

--- script.js
+++ script.js
@@ -23,4 +23,34 @@ dropdown.onchange=function(event){
     }
 }
 
-let
+const delivery=document.querySelector(".dropdown-delivery");
+let address= document.getElementById("address").innerHTML;
+const submit=document.querySelector("#submit");
+
+submit.addEventListener("click", function(){
+    alert("Order received");
+})
+
+/*grand pize of pizza=(topping+crust+size)*quantity
+if place order is cliked dispaly grand prize*/
+
+var small=document.getElementById("small").value;
+var medium=document.getElementById("medium").value;
+var large=document.getElementById("large").value;
+
+small=300;
+medium=550;
+large=800;
+
+const pizzaSize=[small,medium,large];
+
+//console.log(pizzaSize);
+
+var crustCrisp=document.getElementById()
+
+
+
+
+
+
+
